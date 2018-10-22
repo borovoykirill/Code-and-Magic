640922fb915ab7561574c4190d736daf8a8e0110
Borovoy Kirill <borovoy.kirill@yandex.by>
2018-10-22 16:51:51 +0300

    Created a canvas for when you won.

---
+++ js/stat.js
@@ -0,0 +1,41 @@
+var CLOUD_WIDTH = 500;
+var CLOUD_HEIGHT = 180;
+var CLOUD_X = 100;
+var CLOUD_Y = 50;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 20;
+var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+
+var renderCloud = function(ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+    var maxElement = arr[0];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    return maxElement;
+};
+
+window.renderStatistics = function(ctx, players, times) {
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+    ctx.fillStyle = '#5473F7';
+
+    var maxTime = getMaxElement(times);
+
+    for (var i = 0; i < players.length; i++) {
+        ctx.fillText('Ура, Вы победили!', 260, 200);
+        ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
+        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    }
+};
